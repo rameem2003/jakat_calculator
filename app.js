c3025bd6c24faaf2e3ad19a4c4fc20b001c9c925
rameem2003 <rameem2019@gmail.com>
2022-03-27 22:32:05 +0600

    beta release

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var calculate = document.getElementById("calculate");
+var popup_close = document.getElementById("popup_close");
+var reset = document.getElementById("reset");
 
 calculate.addEventListener("click", (e) => {
     e.preventDefault();
 
     var nisab = 43000;
-
+    // variable for all inputs
     var gold_amo = Number(document.getElementById("gold_amo").value);
     var silver_amo = Number(document.getElementById("silver_amo").value);
     var bank = Number(document.getElementById("bank").value);
@@ -16,7 +18,14 @@ calculate.addEventListener("click", (e) => {
     var worker_salary = Number(document.getElementById("worker_salary").value);
     var tax = Number(document.getElementById("tax").value);
 
+    // variable for popup msg
+    var popup_msg_box = document.getElementById("popup_msg_box");
+    var msg_title = document.getElementById("msg_title");
+    var print_amo = document.getElementById("print_amo");
+    var achive = document.querySelector(".achive");
 
+
+    // variables for result
     var asset = gold_amo + silver_amo + bank + save + loan + invest;
     var liabilities = credit + worker_salary + tax;
 
@@ -24,10 +33,36 @@ calculate.addEventListener("click", (e) => {
 
     if(total_asset >= nisab){
         var zakat = (total_asset * 2.5) / 100;
-        alert(zakat.toFixed(2) +  "You can pay jakat");
+        popup_msg_box.style.zIndex = "1";
+        popup_msg_box.style.opacity = "1";
+        achive.classList.replace("fa-circle-xmark", "fa-circle-check");
+        msg_title.innerHTML = "অভিনন্দন";
+        print_amo.innerHTML = "আপনার জাকাতের পরিমান " + zakat + " টাকা";
     }else{
-        alert("You can't pay jakat");
+        popup_msg_box.style.zIndex = "1";
+        popup_msg_box.style.opacity = "1";
+        achive.classList.replace("fa-circle-check", "fa-circle-xmark");
+        achive.style.color = "red";
+        msg_title.innerHTML = "OPPS!!!";
+        print_amo.innerHTML = "আপনার জাকাত দেওয়ার মত পর্যাপ্ত নিসাব নাই!! ";
     }
 
     
+});
+
+
+
+
+popup_close.addEventListener("click", () => {
+    var popup_msg_box = document.getElementById("popup_msg_box");
+
+    popup_msg_box.style.opacity = "0";
+    popup_msg_box.style.zIndex = "-2";
+});
+
+
+
+
+reset.addEventListener("click", () => {
+    document.location.reload();
 });
